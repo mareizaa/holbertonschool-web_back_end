ac6464124ac424ff6a13bb7c2e369251a63a39d9
mareizaa <marce.3210@gmail.com>
2022-02-13 12:18:51 -0500

    Styles 5-7

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' 
+'''
 Takes a list mxd_lst of floats and integers and
 returns their sum as a float.
 '''
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-''' 
+'''
 Takes a string k and an int OR float v as arguments and
 returns a tuple.
 '''
