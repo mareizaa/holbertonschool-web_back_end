c974b318eebc7a0f444cc1531b3e6db383428070
mareizaa <marce.3210@gmail.com>
2022-02-13 12:07:22 -0500

    task 0-2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+Returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Return concatenation of str1 and str2. '''
+    return str1 + str2
